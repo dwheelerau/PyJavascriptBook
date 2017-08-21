546e7185562ed06a5bca08483249c23b31d52a3a
Dave Wheeler <dwheelerau@gamil.com>
2017-08-21 22:04:20 +1200

    pg 37

--- script.js
+++ script.js
@@ -9,33 +9,15 @@ console.log('Sum =' + sum);
 console.log('Hello world');
 
 var foo = {'bar': 3, 'baz': 5};
+var data = [3,4,5];
 
 console.log(foo.bar);
-console.log(foo['baz']);
+console.log(foo.baz);
 
 // example of loop not used very often anymore
-for (var prop in foo) {
-        console.log(prop);
-}
-
+data.forEach(function(d){
+    console.log(d);
+});
 // more modern way using underscore
 //_.each(foo, function(value, key){
 //})
-
-var value1 = 3;
-var value2 = 2;
-// example of js switch
-switch(data[0]){
-    case value1:
-        // excute if expression eq value
-        console.log('will print');
-        break;
-    case value2:
-        // excute if ...
-        break;
-    default:
-        // if all else fails
-}
-
-
-
