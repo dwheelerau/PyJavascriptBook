8c6f7eae1d0b02181d734305fadde70167fb1356
Dave Wheeler <dwheelerau@gmail.com>
2017-08-22 12:18:42 +1200

    update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // script.js
+/* eslint-disable no-var */
 var data = [3, 4];
 var sum = 0;
-data.forEach(function(d){
+data.forEach(function(d) {
     sum += d;
 });
 
@@ -9,16 +10,12 @@ console.log('Sum =' + sum);
 console.log('Hello world');
 
 var foo = {'bar': 3, 'baz': 5};
-var data = [3,4,5];
+var data = [3, 4, 5];
 
 console.log(foo.bar);
 console.log(foo.baz);
 
 // example of loop not used very often anymore
-Object.keys(foo).forEach(function(b){
+Object.keys(foo).forEach(function(b) {
     console.log(b);
 });
-
-// more modern way using underscore
-//_.each(foo, function(value, key){
-//})
